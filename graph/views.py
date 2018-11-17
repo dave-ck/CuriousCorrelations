c9761edae4c88607fe3793e18b3e8fe571c8226b
Sax <emmbluey@gmail.com>
2018-11-17 23:05:57 +0000

    impliamented Holly's html into home.html

--- graph/views.py
+++ graph/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    return render(request, 'graph/home.html')
 
 def about(request):
     return HttpResponse('<h1>About</h1>')
