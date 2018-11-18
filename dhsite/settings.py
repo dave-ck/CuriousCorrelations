b5ef061aedff6b61fb31b7b9c439fa0f63952506
Sax <emmbluey@gmail.com>
2018-11-18 09:53:34 +0000

    hopefully works

--- dhsite/settings.py
+++ dhsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')sj7*fpa@_oy01!!9!j-na6cx0v#0xp(h5uz6qc^rz5ch=jnop'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://curiouscorrelations.herokuapp.com/']
 
 
 # Application definition
